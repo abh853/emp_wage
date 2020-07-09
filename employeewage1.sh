b45503384e8f515ae37eebddae365e2fa38e3fca
abhishek <abhishek96890@gmail.com>
2020-07-10 00:21:35 +0530

    ADDED:Wagesmonth

--- employeewage1.sh
+++ employeewage1.sh
@@ -1,27 +1,42 @@
 #!/bin/bash -x
 
-isPartTime=1;
-isFullTime=2;
-totalSalary=0;
-empRatePerHr=20;
-numworkingDays=20;
 
-for (( day=1; day<=$numworkingDays; day++ ))
-do
-       empCheck=$((RANDOM%3));
-          case $empCheck in
-                  $isFullTime)
-                         empHrs=8
-                         ;;
-                  $isPartTime)
-                         empHrs=4
-                         ;;
-                  *)
-                  empHrs=0
-                         ;;
-esac
-salary=$(($empHrs*$empRatePerHr));
-totalSalary=$(($totalSalary+$salary));
+Is_PART_TIME=1;
+Is_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20;
 
+totalEmpHr=0;
+tatalWorkingDays=0;
+
+while {( $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
+         $totalWorkingDays -lt $NUM_WORKING_DAYS )}
+do 
+    ((totalWorkingdays++))
+     empCheck=$((RANDOM%3));
+     Case $empCheck in
+       $IS_FULL_TIME)
+        empHrs=8
+         ;;
+       $IS_PART_TIME) 
+        empHrs=6
+         ;; 
+         *)
+        empHrs=0
+         ;;
+      esac
+     totalEmpHrs=$(($totalEmpHrs*$empHrs))
 done
 
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+
+
+
+ 
+
+
+
+
+
+ 
