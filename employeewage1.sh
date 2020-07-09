729206491d66752e1d9f189a3a52ea3fb3ddbb6b
abhishek <abhishek96890@gmail.com>
2020-07-09 22:20:48 +0530

    ADDED:Dailwage

--- employeewage1.sh
+++ employeewage1.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
 
-Employee=$(($RANDOM%2))
-if [ $Employee -eq 1 ]
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ];
 then
-		echo "Employee is present"
+        empRatePerHr=20;
+        empHrs=8;
+        salary=$(($empHrs*$empRatePerHr));
 else
-		echo "Employee is absent"
-fi
+  salary=0;
+fi		
 
